f3d6c2b1a906dc6b52b7ff8341b8e291fd71c0f8
Jaime Ribeiro Barrancos <jaime.barrancos@outlook.com>
2023-05-13 22:54:53 +0100

    push

---
+++ config.ts
@@ -0,0 +1,61 @@
+import fs from 'fs';
+import path from 'path';
+
+const fileLensHub = fs.readFileSync(
+  path.join(__dirname, 'abis/lens-hub-contract-abi.json'),
+  'utf8'
+);
+const fileLensPeriphery = fs.readFileSync(
+  path.join(__dirname, 'abis/lens-periphery-data-provider.json'),
+  'utf8'
+);
+const fileFollowNFT = fs.readFileSync(
+  path.join(__dirname, 'abis/lens-follow-nft-contract-abi.json'),
+  'utf8'
+);
+
+const getParamOrExit = (name: string) => {
+  const param = process.env[name];
+  if (!param) {
+    console.error(`Required config param '${name}' missing`);
+    process.exit(1);
+  }
+  return param;
+};
+
+const getParam = (name: string) => {
+  const param = process.env[name];
+  if (!param) {
+    return null;
+  }
+  return param;
+};
+
+export const explicitStart = (filename: string) => {
+  const scriptName = path.basename(process.argv[1]);
+  return path.basename(filename).includes(scriptName);
+};
+
+export const PK = getParamOrExit('PK');
+
+export const MUMBAI_RPC_URL = getParamOrExit('MUMBAI_RPC_URL');
+
+export const LENS_API = getParamOrExit('LENS_API');
+
+export const LENS_HUB_CONTRACT = getParamOrExit('LENS_HUB_CONTRACT');
+
+export const LENS_PERIPHERY_CONTRACT = getParamOrExit('LENS_PERIPHERY_CONTRACT');
+
+export const LENS_PERIPHERY_NAME = 'LensPeriphery';
+
+export const PROFILE_ID = getParam('PROFILE_ID');
+
+export const LENS_FOLLOW_NFT_ABI = JSON.parse(fileFollowNFT);
+
+export const LENS_HUB_ABI = JSON.parse(fileLensHub);
+
+export const LENS_PERIPHERY_ABI = JSON.parse(fileLensPeriphery);
+
+export const INFURA_PROJECT_ID = getParam('INFURA_PROJECT_ID');
+
+export const INFURA_SECRET = getParam('INFURA_SECRET');
---
+++ login.ts
@@ -0,0 +1,59 @@
+import { apolloClient } from './apollo-client';
+import { explicitStart } from './config';
+import { getAddressFromSigner, signText } from './ethers.service';
+import {
+  AuthenticateDocument,
+  ChallengeDocument,
+  ChallengeRequest,
+  SignedAuthChallenge,
+} from './graphql/generated';
+import { getAuthenticationToken, setAuthenticationToken } from './state';
+
+export const generateChallenge = async (request: ChallengeRequest) => {
+  const result = await apolloClient.query({
+    query: ChallengeDocument,
+    variables: {
+      request,
+    },
+  });
+
+  return result.data.challenge;
+};
+
+const authenticate = async (request: SignedAuthChallenge) => {
+  const result = await apolloClient.mutate({
+    mutation: AuthenticateDocument,
+    variables: {
+      request,
+    },
+  });
+
+  return result.data!.authenticate;
+};
+
+export const login = async (address = getAddressFromSigner()) => {
+  if (getAuthenticationToken()) {
+    console.log('login: already logged in');
+    return;
+  }
+
+  console.log('login: address', address);
+
+  // we request a challenge from the server
+  const challengeResponse = await generateChallenge({ address });
+
+  // sign the text with the wallet
+  const signature = await signText(challengeResponse.text);
+
+  const authenticatedResult = await authenticate({ address, signature });
+  console.log('login: result', authenticatedResult);
+  setAuthenticationToken(authenticatedResult.accessToken);
+
+  return authenticatedResult;
+};
+
+(async () => {
+  if (explicitStart(__filename)) {
+    await login();
+  }
+})();
